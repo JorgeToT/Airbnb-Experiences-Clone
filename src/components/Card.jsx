b92779ded55c55d9badf5bd5c80662be9e94cb4f
Jorge Aguilar <jorge.agvel@gmail.com>
2022-04-16 21:02:08 -0500

    Finished and deploy

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,26 @@
 import star from "../assets/star.png";
 
 function Card(props) {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
   return (
     <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
       <img
-        src={require("../assets/" + props.image)}
+        src={require("../assets/" + props.coverImg)}
         alt=""
         className="card--img"
       />
       <div className="card--info">
         <img src={star} alt="" className="card--star" />
-        <span className="card--rating">{props.rating}</span>
-        <span className="card--reviews gray">({props.reviewCount}) • </span>
+        <span className="card--rating">{props.stats.rating}</span>
+        <span className="card--reviews gray">
+          ({props.stats.reviewCount}) •{" "}
+        </span>
         <span className="card--location gray">{props.location}</span>
       </div>
       <p className="card--tittle">{props.title}</p>
